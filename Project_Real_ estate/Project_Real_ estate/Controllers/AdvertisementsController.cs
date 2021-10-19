a877e6210edab223436f778f0041961daefff0e4
tuankun-279 <59610132+tuankun-279@users.noreply.github.com>
2021-10-19 12:15:10 +0700

    commit

---
+++ Project_Real_ estate/Project_Real_ estate/Controllers/AdvertisementsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project_Real__estate.Models;
+
+namespace Project_Real__estate.Controllers
+{
+    public class AdvertisementsController : Controller
+    {
+        private RealEstateEntities1 db = new RealEstateEntities1();
+
+        // GET: Advertisements
+        public ActionResult Index()
+        {
+            var advertisements = db.Advertisements.Include(a => a.Agent).Include(a => a.Category).Include(a => a.Payment).Include(a => a.Seller).Include(a => a.User);
+            return View(advertisements.ToList());
+        }
+
+        // GET: Advertisements/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advertisement advertisement = db.Advertisements.Find(id);
+            if (advertisement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertisement);
+        }
+
+        // GET: Advertisements/Create
+        public ActionResult Create()
+        {
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName");
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName");
+            ViewBag.PaymentId = new SelectList(db.Payments, "PaymentId", "PaymentName");
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        }
+
+        // POST: Advertisements/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "adsId,Tiltle,ReleaseDate,ExpirationDate,SellerId,AgentId,PaymentId,CategoryId,Describe,CurrentSymbol,priceOfAds,EstatePrice,Facade,Gateway,floors,Bedrooms,Toilets,furniture,Area,Cityprovince,District,Ward,Street,isActivate,UserId,StatusHouse")] Advertisement advertisement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Advertisements.Add(advertisement);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName", advertisement.AgentId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", advertisement.CategoryId);
+            ViewBag.PaymentId = new SelectList(db.Payments, "PaymentId", "PaymentName", advertisement.PaymentId);
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name", advertisement.SellerId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", advertisement.UserId);
+            return View(advertisement);
+        }
+
+        // GET: Advertisements/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advertisement advertisement = db.Advertisements.Find(id);
+            if (advertisement == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName", advertisement.AgentId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", advertisement.CategoryId);
+            ViewBag.PaymentId = new SelectList(db.Payments, "PaymentId", "PaymentName", advertisement.PaymentId);
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name", advertisement.SellerId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", advertisement.UserId);
+            return View(advertisement);
+        }
+
+        // POST: Advertisements/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "adsId,Tiltle,ReleaseDate,ExpirationDate,SellerId,AgentId,PaymentId,CategoryId,Describe,CurrentSymbol,priceOfAds,EstatePrice,Facade,Gateway,floors,Bedrooms,Toilets,furniture,Area,Cityprovince,District,Ward,Street,isActivate,UserId,StatusHouse")] Advertisement advertisement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(advertisement).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName", advertisement.AgentId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", advertisement.CategoryId);
+            ViewBag.PaymentId = new SelectList(db.Payments, "PaymentId", "PaymentName", advertisement.PaymentId);
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name", advertisement.SellerId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", advertisement.UserId);
+            return View(advertisement);
+        }
+
+        // GET: Advertisements/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advertisement advertisement = db.Advertisements.Find(id);
+            if (advertisement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertisement);
+        }
+
+        // POST: Advertisements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Advertisement advertisement = db.Advertisements.Find(id);
+            db.Advertisements.Remove(advertisement);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Project_Real_ estate/Project_Real_ estate/Controllers/AgentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project_Real__estate.Models;
+
+namespace Project_Real__estate.Controllers
+{
+    public class AgentsController : Controller
+    {
+        private RealEstateEntities1 db = new RealEstateEntities1();
+
+        // GET: Agents
+        public ActionResult Index()
+        {
+            var agents = db.Agents.Include(a => a.User).Include(a => a.Payment);
+            return View(agents.ToList());
+        }
+
+        // GET: Agents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agent agent = db.Agents.Find(id);
+            if (agent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agent);
+        }
+
+        // GET: Agents/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            ViewBag.paymentId = new SelectList(db.Payments, "PaymentId", "PaymentName");
+            return View();
+        }
+
+        // POST: Agents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AgentId,AgentName,Email,Address,Phone,isActivate,Password,Introduction,EmailHide,paymentId,UserId")] Agent agent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agents.Add(agent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", agent.UserId);
+            ViewBag.paymentId = new SelectList(db.Payments, "PaymentId", "PaymentName", agent.paymentId);
+            return View(agent);
+        }
+
+        // GET: Agents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agent agent = db.Agents.Find(id);
+            if (agent == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", agent.UserId);
+            ViewBag.paymentId = new SelectList(db.Payments, "PaymentId", "PaymentName", agent.paymentId);
+            return View(agent);
+        }
+
+        // POST: Agents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AgentId,AgentName,Email,Address,Phone,isActivate,Password,Introduction,EmailHide,paymentId,UserId")] Agent agent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", agent.UserId);
+            ViewBag.paymentId = new SelectList(db.Payments, "PaymentId", "PaymentName", agent.paymentId);
+            return View(agent);
+        }
+
+        // GET: Agents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agent agent = db.Agents.Find(id);
+            if (agent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agent);
+        }
+
+        // POST: Agents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agent agent = db.Agents.Find(id);
+            db.Agents.Remove(agent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
